e03eeea0443ddaae94acd3fc5f4f60fa243c9919
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-20 15:52:49 +0200

    Declare a label variable and assign it a new div element

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onload = () => {
     const container = document.getElementById("container");
     const createLabel = (name) => {
-        
+        const label = document.createElement("div");
+
     }
 
   

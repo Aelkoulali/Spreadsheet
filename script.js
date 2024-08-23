52f091258bfc0e77a587e93b59d3643b088e9e0b
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-23 09:52:50 +0200

    Update and return the value of that input element

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).
 
 // Dclare Function to parse and evaluate the input string
 const evaFormula = (x, cells) => {
-    const idToText = (id) => cells.find((cell) => cell.id === id); 
+    const idToText = (id) => cells.find((cell) => cell.id === id).value; 
 
 };
 

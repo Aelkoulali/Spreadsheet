8bf53c0aaae5468a801e086e67aecc840ccb23b9
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-25 13:26:24 +0200

    Declare infixToFunction

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Declare infixToFunction 
+const infixToFunction = {
+    
+};
+
 // Declare isEven function, return true if num is even, false otherwise 
 const isEven = (num) => num % 2 === 0;
 
@@ -37,7 +42,7 @@ const evaFormula = (x, cells) => {
     const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
     const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2 ) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
     const cellRegex = /[A-J][1-9][0-9]?/gi;  
-    const cellExpanded = rangeExpanded.replace(cellRegex, (match)=> idToText(match.toUpperCase()));
+    const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
 };
       
 

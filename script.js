f2ad39ae881fe9975b6eb1758e03bd9d9b715671
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-26 09:40:11 +0200

    Add new propreties firsttwo, lasttwo

--- script.js
+++ script.js
@@ -38,6 +38,9 @@ const spreadsheetFunctions = {
     average,
     median,
     even : nums => nums.filter(isEven),
+    firsttwo: nums => nums.slice(0,2),
+    lasttwo: nums => nums.slice(nums.length - 2),
+
 };
 
 // Declare applyFunction
